91d15d26b610804b0f77a86f03252d0604c7c5ff
Ahmet Türkmen <turkmenahmettalha@gmail.com>
2022-08-10 15:28:25 +0300

    Explode Sequence

--- Fruit Ninja Clone/Assets/Scripts/GameManager.cs
+++ Fruit Ninja Clone/Assets/Scripts/GameManager.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 
 public class GameManager : MonoBehaviour
 {
     public TextMeshProUGUI scoreText;
+    public Image fadeImage;
 
     private int score;
 
@@ -25,8 +27,30 @@ public class GameManager : MonoBehaviour
 
     void NewGame()
     {
+        Time.timeScale = 1f;
+
+        blade.enabled = true;
+        spawner.enabled = true;
+
         score = 0;
         scoreText.text = score.ToString();
+
+        ClearScene();
+    }
+
+    void ClearScene()
+    {
+        Fruit[] fruits = FindObjectsOfType<Fruit>();
+        foreach (Fruit fruit in fruits)
+        {
+            Destroy(fruit.gameObject);
+        }
+
+        Bomb[] bombs = FindObjectsOfType<Bomb>();
+        foreach (Bomb bomb in bombs)
+        {
+            Destroy(bomb.gameObject);
+        }
     }
 
     public void IncreaseScore(int amount)
@@ -39,5 +63,40 @@ public class GameManager : MonoBehaviour
     {
         blade.enabled = false;
         spawner.enabled = false;
+
+        StartCoroutine(ExplodeSequence());
+    }
+
+    IEnumerator ExplodeSequence()
+    {
+        float elapsed = 0f;
+        float duration = 0.5f;
+
+        while (elapsed < duration)
+        {
+            float t = Mathf.Clamp01(elapsed / duration);
+            fadeImage.color = Color.Lerp(Color.clear, Color.white, t);
+
+            Time.timeScale = 1f - t;
+            elapsed += Time.unscaledDeltaTime;
+
+            yield return null;
+        }
+
+        yield return new WaitForSecondsRealtime(1f);
+
+        NewGame();
+
+        elapsed = 0f;
+
+        while (elapsed < duration)
+        {
+            float t = Mathf.Clamp01(elapsed / duration);
+            fadeImage.color = Color.Lerp(Color.white, Color.clear, t);
+
+            elapsed += Time.unscaledDeltaTime;
+
+            yield return null;
+        }
     }
 }
